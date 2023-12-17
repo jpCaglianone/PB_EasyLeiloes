4331080243bdcb45ecdedc6742b3f8c542ecfdf2
PC <joao.caglianone@gmail.com>
2023-12-17 17:40:14 -0300

    esqueleto do projeto

---
+++ backend/src/main/java/com/example/PB_EasyLeiloes/PbEasyLeiloesApplication.java
@@ -0,0 +1,13 @@
+package com.example.PB_EasyLeiloes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbEasyLeiloesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbEasyLeiloesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/PB_EasyLeiloes/PbEasyLeiloesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PB_EasyLeiloes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbEasyLeiloesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
